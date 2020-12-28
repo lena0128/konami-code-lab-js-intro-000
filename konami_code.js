ca68f5d104cc4377fd5961d2f00dd38ae893d9c9
Lena Hou <lenahou2013@gmail.com>
2020-12-28 20:17:44 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,26 @@ const codes = [
   "a"
 ];
 
+const myCodes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
 function init() {
-  // your code here
+
+var index = 0;
+
+document.body.addEventListener('keydown', onKeyDownHandler);
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+
+  if (key == myCodes[index]) {
+    index++;
 }
+    if (myCodes.length === index) {
+      alert ("YOU DID IT!");
+      index = 0;
+  } else {
+    index = 0;
+  }
+}
+}
+init();
