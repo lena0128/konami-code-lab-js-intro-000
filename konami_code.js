9efae6676ce46889631d1b31a0237d780357bc5f
Lena Hou <lenahou2013@gmail.com>
2020-12-28 20:33:22 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,36 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+
+  document.body.addEventListener("keydown", keyHandler);
+
+  var myCodes =   [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+
+var myIndex = 0;
+
+  function keyHandler(event) {
+  const key = event.key;
+  if(myCodes[myIndex] === key) {
+  myIndex++;
+
+  if(myCodes.length === myIndex) {
+  window.alert("Hurray");
+  myIndex = 0;
+ }
+
+} else {
+ myIndex = 0;
+  }
+ }
 }
